c63ea66d7b65e2394c42979c92f6601b17bc65de
ceromova <celestemvalera@gmail.com>
2023-03-30 22:00:58 +0200

    Add questions

--- src/Question.js
+++ src/Question.js
@@ -3,40 +3,50 @@ import Motivation from "./Motivation.json"
 
 const randomIndex = (q) => Math.floor(Math.random()*q.length)
 
-const Quiz = ({questions}) => {
-  const [questionIndex, setQuestionIndex] = useState(randomIndex(questions)); /* Initial question */
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const Quiz = ({questions, numQuestions = 10}) => {
+  const [questionIndex, setQuestionIndex] = useState(0); /* Initial question */
   const [selectedAnswer, setSelectedAnswer] = useState(-1);
   const [score, setScore] = useState(0);
-  const randomQuestion = useRef([...questions]); /* ... hace una copia del nuevo array con todos los elementos del anterior array */
-  const [correctAnswer, setCorrectAnswer] = useState()
+  const [correctAnswer, setCorrectAnswer] = useState();
+
+  const randomQuestions = useMemo(() => shuffle([...questions]).slice(0, numQuestions), [questions, numQuestions]);
+
+  const randomQuestion = useRef([...randomQuestions]); /* ... hace una copia del nuevo array con todos los elementos del anterior array */
 
   const handleAnswerSelection = (index) => {
     setSelectedAnswer(index);
   };
 
-const loadNextQuestion = (isCorrect) => {
-  setScore(isCorrect ? score+1 : score)
-  randomQuestion.current.splice(questionIndex,1)
-  // console.log(randomQuestion.current)
-  setSelectedAnswer(-1);
-  setQuestionIndex(randomIndex(randomQuestion.current));
-}
+  const loadNextQuestion = (isCorrect) => {
+    setScore(isCorrect ? score + 1 : score)
+    randomQuestion.current.splice(questionIndex, 1)
+    setSelectedAnswer(-1);
+    setQuestionIndex(randomIndex(randomQuestion.current));
+  }
 
   const handleNextQuestion = () => {
     if (correctAnswer) {
       setCorrectAnswer()
       loadNextQuestion(false)
-      return 
+      return
     }
-    
+
     const questionPool = randomQuestion.current[questionIndex]
     const isCorrect = selectedAnswer === questionPool.correctAnswerIndex
-    
-    if (!isCorrect) {
-    setCorrectAnswer(questionPool.choices[questionPool.correctAnswerIndex])
-    }
-    else loadNextQuestion(true)
 
+    if (!isCorrect) {
+      setCorrectAnswer(questionPool.choices[questionPool.correctAnswerIndex])
+    } else {
+      loadNextQuestion(true)
+    }
   };
 
   const currentQuestion = randomQuestion.current[questionIndex];
@@ -47,20 +57,22 @@ const loadNextQuestion = (isCorrect) => {
 
       cc.array[cc.indexes[index]] = {answer, index:realindex}
       cc.indexes.splice(index, 1)
-  
+
       return cc
-  
-    }, { indexes:Array.from([...Object.keys(currentQuestion.choices)]), array: new Array(currentQuestion.choices.length)})  
+
+    }, { indexes:Array.from([...Object.keys(currentQuestion.choices)]), array: new Array(currentQuestion.choices.length)})
   }, [currentQuestion]) 
 
-if (!currentQuestion){
-  return <>Tu puntuacion es de: {score}
-  <div>
-  {Motivation[randomIndex(Motivation)]}
-  </div>
-  
-  </>
-}
+  if (!currentQuestion) {
+    return (
+      <>
+        Tu puntuación es de: {score}
+        <div>
+          {Motivation[randomIndex(Motivation)]}
+        </div>
+      </>
+    );
+  }
 
   return (
     <div style={{alignSelf:"start"}}>
@@ -72,12 +84,13 @@ if (!currentQuestion){
         </div>
       ))}
       <button disabled={selectedAnswer === -1} onClick={handleNextQuestion}>
-        {correctAnswer?"Siguiente pregunta":"Validar"}
-        {/* Next Question */}
+        {correctAnswer ? "Siguiente pregunta" : "Validar"}
       </button>
-      {correctAnswer && <div className='Answer'>
-        La respuesta correcta es: {correctAnswer}
-      </div>}
+      {correctAnswer && (
+        <div className='Answer'>
+          La respuesta correcta es: {correctAnswer}
+        </div>
+      )}
     </div>
   );
 };
